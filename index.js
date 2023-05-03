911e7fb92fc8421d47101718a04392d4bdd2420e
Cody Roberts <pithlyx@gmail.com>
2023-05-03 12:48:34 -0600

    added dom content loaded function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let h1Element = document.querySelector("#text")
+document.addEventListener("DOMContentLoaded", function(e) 
+    {
+        console.log(e)
+        h1Element.textContent = "This is really cool!"
+    })
